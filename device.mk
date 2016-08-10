nitin1438/android_device_lge_d855

init.d: add boot cleaner
* based on https://github.com/PAC-ROM/android_vendor_pac/blob/pac-4.4/prebuilt/common/etc/init.d/81removecache
  all credit to PAC-ROM

Change-Id: Ife6457910dcca2e4dc751613eaf23662e210bca8

device.mk
@@ -83,5 +83,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     GloveMode
 
+# Boot Cleaner
+PRODUCT_COPY_FILES += \
+    device/lge/d855/prebuilt/etc/init.d/81clean:system/etc/init.d/81clean
+
 # Common G3
 $(call inherit-product, device/lge/g3-common/g3.mk)

Split-Screen/android_device_xiaomi_kenzo

device.mk
@@ -271,3 +271,7 @@ PRODUCT_COPY_FILES += \
 # Dalvik-Hack
 PRODUCT_COPY_FILES += \
     device/xiaomi/kenzo/prebuilt/etc/init.d/01dalvik:system/etc/init.d/01dalvik
+
+# Boot Cleaner
+PRODUCT_COPY_FILES += \
+    device/xiaomi/kenzo/prebuilt/etc/init.d/81clean:system/etc/init.d/81clean